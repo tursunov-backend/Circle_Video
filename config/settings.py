e576850deb93bc70b6d23d606d5b05946e5aa407
tursunov-backent <donitursunov98@gmail.com>
2026-07-08 22:36:27 +0500

    templates

--- config/settings.py
+++ config/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [ BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -127,6 +127,15 @@ USE_I18N = True
 USE_TZ = True
 
 
+STATIC_URL = "static/"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "media/"
+    
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
